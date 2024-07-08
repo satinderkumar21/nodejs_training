03e843fc3db554ac0ac93a01af112b962a75b8d5
satinderkumar21 <satinder21ku@gmail.com>
2024-07-08 21:39:16 +0530

    crud operation

---
+++ crud2/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+fs.mkdir('satinder', () => {
+    console.log("folder has created");
+});
+
+
+fs.writeFile('./satinder/bio.txt', 'my name is satinder kumar', () =>{
+    console.log("file has created ");
+});
+
+fs.rename('./satinder/bio.txt','./satinder/mybio.txt', (err) =>{
+    console.log("file name has changed");
+}  )
+
+fs.appendFile('./satinder/mybio.txt', 'my age is 22. i am from ldh', (err) =>{
+    console.log('data is add to file');
+})
+
+
+fs.readFile('./satinder/mybio.txt', 'utf8', (err, data) => {
+console.log(data);
+})
+
+fs.unlink('./satinder/mybio.txt', () => {
+    console.log("file has deleted");
+});
+
+
+
